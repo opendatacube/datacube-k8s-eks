110fa092f1fe1f127bfdc5abe2d4a511ef56660c
Woodcock, Robert (Mineral Resources, Black Mountain) <Robert.Woodcock@csiro.au>
2019-05-17 16:30:42 +1000

    Add the ability to specify a workspace path so a separate repo can be used for workspace configurations

--- Makefile
+++ Makefile
@@ -2,24 +2,24 @@ create-servers:
 	packer build packer/compute.packer.json
 
 init:
-	./create.sh $(cluster)
+	./create.sh $(cluster) $(workspaces)
 
 destroy:
-	./destroy.sh $(cluster)
+	./destroy.sh $(cluster) $(workspaces)
 
 patch:
 	@cd infra; \
-	./deploy.sh $(cluster);
+	./deploy.sh $(cluster) $(workspaces);
 
 setup:
 	@cd helm; \
-	/bin/bash deploy.sh $(cluster);
+	/bin/bash deploy.sh $(cluster) $(workspaces);
 	@cd scripts; \
 	/bin/bash ./create-cluster-defaults-secret.sh
 
 setup-orchestration:
 	@cd infra/orchestration; \
-	./deploy.sh $(cluster)
+	./deploy.sh $(cluster) $(workspaces)
 
 # use like "make run template=index-job name=nrt"
 run-index:
@@ -39,4 +39,4 @@ test:
 
 test-infra:
 	@cd infra; \
-	./test.sh $(cluster)
+	./test.sh $(cluster) $(workspaces)
--- create.sh
+++ create.sh
@@ -6,18 +6,19 @@ set -e
 # Patch EKS nodes
 # 
 
-if [ -z $1 ]; then
-    echo "Error: cluster variable is not set, I don't know which cluster you want to create"
+if [ -z $1 ] || [ -z $2 ]; then
+    echo "Error: workspace and/or workspace path variables not set, I don't know which cluster you want to create"
     exit 1
 fi
 
 export WORKSPACE=$1
+export WORKSPACESPATH=$2
 
 # build network and EKS masters
 pushd infra
-rm -rf .terraform
-terraform init -backend-config ../workspaces/$WORKSPACE/backend.cfg 
-terraform apply -auto-approve -input=false -var-file="../workspaces/$WORKSPACE/terraform.tfvars" 
+# rm -rf .terraform
+terraform init -backend-config $WORKSPACESPATH/$WORKSPACE/backend.cfg 
+terraform apply -auto-approve -input=false -var-file="$WORKSPACESPATH/$WORKSPACE/terraform.tfvars" 
 
 # Configure local kubernetes config
 aws eks --region $(terraform output region) update-kubeconfig --name $(terraform output cluster_name)
@@ -29,10 +30,10 @@ popd
 
 # build worker nodes
 pushd nodes
-rm -rf .terraform
-terraform init -backend-config ../workspaces/$WORKSPACE/backend.cfg 
+# rm -rf .terraform
+terraform init -backend-config $WORKSPACESPATH/$WORKSPACE/backend.cfg 
 terraform workspace new "$WORKSPACE-blue" || terraform workspace select "$WORKSPACE-blue"
-terraform apply -auto-approve -input=false -var-file="../workspaces/$WORKSPACE/terraform.tfvars" 
+terraform apply -auto-approve -input=false -var-file="$WORKSPACESPATH/$WORKSPACE/terraform.tfvars" 
 popd
 
 pushd infra
@@ -43,8 +44,8 @@ kubectl apply -f tiller.yaml
 popd
 
 pushd addons
-rm -rf .terraform
-terraform init -backend-config ../workspaces/$WORKSPACE/backend.cfg 
+# rm -rf .terraform
+terraform init -backend-config $WORKSPACESPATH/$WORKSPACE/backend.cfg 
 terraform workspace new "$WORKSPACE-addons" || terraform workspace select "$WORKSPACE-addons"
-terraform apply -auto-approve -input=false -var-file="../workspaces/$WORKSPACE/terraform.tfvars" 
+terraform apply -auto-approve -input=false -var-file="$WORKSPACESPATH/$WORKSPACE/terraform.tfvars" 
 popd
