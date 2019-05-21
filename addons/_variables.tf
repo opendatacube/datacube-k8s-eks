a102cf5ec3a1c2a87df67d1816e01427e84b9e4a
Rob Woodcock <woodcockr@users.noreply.github.com>
2019-05-21 11:07:04 +1000

    Update addons/_variables.tf
    Co-Authored-By: tom-butler <tombutler.development@gmail.com>

--- addons/_variables.tf
+++ addons/_variables.tf
@@ -4,7 +4,7 @@ variable "domain_name" {
   description = "The domain name to be used by for applications deployed to the cluster and using ingress"
   type        = "string"
 }
-variable "txtOwnerId" {
+variable "txt_owner_id" {
   description = "When using the TXT registry, a name that identifies this instance of ExternalDNS"
   default = "AnOwnerId"
 }
