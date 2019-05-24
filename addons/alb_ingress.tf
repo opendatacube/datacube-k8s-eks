0e85fe284704a33f466b576b0ba8030f421a01b4
tom-butler <tombutler.development@gmail.com>
2019-05-24 13:27:48 +1000

    Prometheus grafana addon (#33)
    * Pin helm charts to repo, so they don't re-install every run
    
    * Add Prometheus with ssl enabled grafana

--- addons/alb_ingress.tf
+++ addons/alb_ingress.tf
@@ -212,3 +212,32 @@ resource "aws_iam_role_policy" "alb" {
 }
 EOF
 }
+
+# Create a wildcard cert for use on the alb
+resource "aws_acm_certificate" "wildcard_cert" {
+  count             = "${var.alb_ingress_enabled}"
+  domain_name       = "*.${var.domain_name}"
+  validation_method = "DNS"
+}
+
+# Automatically validate the cert using DNS validation
+data "aws_route53_zone" "wildcard_zone" {
+  count        = "${var.alb_ingress_enabled}"
+  name         = "${var.domain_name}"
+  private_zone = false
+}
+
+resource "aws_route53_record" "wildcard_cert_validation" {
+  count   = "${var.alb_ingress_enabled}"
+  name    = "${aws_acm_certificate.wildcard_cert.domain_validation_options.0.resource_record_name}"
+  type    = "${aws_acm_certificate.wildcard_cert.domain_validation_options.0.resource_record_type}"
+  zone_id = "${data.aws_route53_zone.wildcard_zone.id}"
+  records = ["${aws_acm_certificate.wildcard_cert.domain_validation_options.0.resource_record_value}"]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "wildcard_cert" {
+  count                   = "${var.alb_ingress_enabled}"
+  certificate_arn         = "${aws_acm_certificate.wildcard_cert.arn}"
+  validation_record_fqdns = ["${aws_route53_record.wildcard_cert_validation.fqdn}"]
+}
---
+++ addons/prometheus.tf
@@ -0,0 +1,54 @@
+# Prometheus - View and alert on metrics
+# ==================================
+
+variable "prometheus_enabled" {
+  default = false
+}
+
+resource "kubernetes_namespace" "monitoring" {
+  count = "${var.prometheus_enabled ? 1 : 0}"
+
+  metadata {
+    name = "monitoring"
+
+    labels {
+      managed-by = "Terraform"
+    }
+  }
+}
+
+# Create the prometheus operator, configure grafana dashboard ingress
+resource "helm_release" "prometheus_operator" {
+  count      = "${var.prometheus_enabled  ? 1 : 0}"
+  name       = "prometheus-operator"
+  repository = "stable"
+  chart      = "prometheus-operator"
+  namespace  = "monitoring"
+
+  values = [<<EOF
+grafana:
+  ingress:
+    enabled: ${var.alb_ingress_enabled}
+    annotations:
+      kubernetes.io/ingress.class: alb
+      alb.ingress.kubernetes.io/scheme: internet-facing
+      alb.ingress.kubernetes.io/target-type: ip
+      alb.ingress.kubernetes.io/healthcheck-path: "/metrics"
+      alb.ingress.kubernetes.io/certificate-arn: "${aws_acm_certificate_validation.wildcard_cert.0.certificate_arn}"
+      alb.ingress.kubernetes.io/listen-ports: '[{"HTTP": 80}, {"HTTPS":443}]'
+    hosts: 
+      - mgmt.${var.domain_name}
+    path: /*
+  env:
+    GF_SERVER_ROOT_URL: https://mgmt.${var.domain_name}/
+  # force chart to generate password as prometheus-operator-grafana secret
+  adminPassword: null
+EOF
+  ]
+
+  depends_on = ["kubernetes_namespace.monitoring",
+    "kubernetes_service_account.tiller",
+    "kubernetes_cluster_role_binding.tiller_clusterrolebinding",
+    "null_resource.helm_init_client",
+  ]
+}
