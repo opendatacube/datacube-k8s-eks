1a1e108bef2114e6c95bc50dec6aec16d4ae4dfb
Robert Woodcock <Robert.Woodcock@csiro.au>
2019-06-06 15:53:16 +1000

    Remove excess TF-UPGRADE-TOTO comment that work fine now

--- infra/modules/eks/master_sg.tf
+++ infra/modules/eks/master_sg.tf
@@ -18,14 +18,6 @@ resource "aws_security_group" "eks-cluster" {
 # Converts admin_access_CIDRs to descriptions / IP CIDRs
 resource "aws_security_group_rule" "eks-cluster-ingress-workstation-https" {
   count = length(var.admin_access_CIDRs)
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibilty in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
   cidr_blocks       = [element(values(var.admin_access_CIDRs), count.index)]
   description       = element(keys(var.admin_access_CIDRs), count.index)
   from_port         = 443
--- infra/modules/eks/user_policy.tf
+++ infra/modules/eks/user_policy.tf
@@ -18,14 +18,6 @@ data "aws_iam_policy_document" "assume_role" {
     # List of users
     principals {
       type = "AWS"
-      # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-      # force an interpolation expression to be interpreted as a list by wrapping it
-      # in an extra set of list brackets. That form was supported for compatibilty in
-      # v0.11, but is no longer supported in Terraform v0.12.
-      #
-      # If the expression in the following list itself returns a list, remove the
-      # brackets to avoid interpretation as a list of lists. If the expression
-      # returns a single list item then leave it as-is and remove this TODO comment.
       identifiers = local.accounts_arn
     }
 
