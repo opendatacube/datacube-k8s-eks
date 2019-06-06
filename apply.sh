0472c4edc3b5d7112bf593a2063666d6d44d8bf6
Robert Armstrong <robert.armstrong@ga.gov.au>
2019-06-06 11:50:23 +1000

    Use plan -> apply workflow in apply and patch scripts

--- apply.sh
+++ apply.sh
@@ -23,11 +23,13 @@ pushd infra
 if [ ! -z "$CLEAN" ]; then
     rm -rf .terraform
 fi
-terraform init -backend-config $WORKSPACESPATH/$WORKSPACE/backend.cfg 
-terraform apply -auto-approve -input=false -var-file="$WORKSPACESPATH/$WORKSPACE/terraform.tfvars" 
+terraform init -backend-config $WORKSPACESPATH/$WORKSPACE/backend.cfg
+terraform plan -out infra.plan -input=false -var-file="$WORKSPACESPATH/$WORKSPACE/terraform.tfvars"
+terraform apply -auto-approve infra.plan
+rm infra.plan
 
 # Configure local kubernetes config
-aws eks --region $(terraform output region) update-kubeconfig --name $(terraform output cluster_name)
+# aws eks --region $(terraform output region) update-kubeconfig --name $(terraform output cluster_name)
 
 # Set up aws-auth
 popd
@@ -41,23 +43,29 @@ fi
 terraform init -backend-config $WORKSPACESPATH/$WORKSPACE/backend.cfg 
 
 terraform workspace new "$WORKSPACE-blue" || terraform workspace select "$WORKSPACE-blue"
-terraform apply -auto-approve -input=false \
+terraform plan -out workersblue.plan -input=false \
     -var-file="$WORKSPACESPATH/$WORKSPACE/terraform.tfvars" \
     -var group_enabled=true \
     -var node_group_name=blue
+terraform apply -auto-approve workersblue.plan
+rm workersblue.plan
 
 terraform workspace new "$WORKSPACE-green" || terraform workspace select "$WORKSPACE-green"
-terraform apply -auto-approve -input=false \
+terraform plan -out workersgreen.plan -input=false \
     -var-file="$WORKSPACESPATH/$WORKSPACE/terraform.tfvars" \
     -var group_enabled=false \
     -var node_group_name=green
+terraform apply -auto-approve workersgreen.plan
+rm workersgreen.plan
 popd
 
 pushd addons
 if [ ! -z "$CLEAN" ]; then
     rm -rf .terraform
 fi
-terraform init -backend-config $WORKSPACESPATH/$WORKSPACE/backend.cfg 
+terraform init -backend-config $WORKSPACESPATH/$WORKSPACE/backend.cfg
 terraform workspace new "$WORKSPACE-addons" || terraform workspace select "$WORKSPACE-addons"
-terraform apply -auto-approve -input=false -var-file="$WORKSPACESPATH/$WORKSPACE/terraform.tfvars" 
+terraform plan -out addons.plan -input=false -var-file="$WORKSPACESPATH/$WORKSPACE/terraform.tfvars"
+terraform apply -auto-approve addons.plan
+rm addons.plan
 popd
