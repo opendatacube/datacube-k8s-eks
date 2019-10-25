58cc244db7087cb5c844ac239947b948b250422c
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2019-10-25 14:26:00 +1100

    added resources required to enable logging

--- addons/_variables.tf
+++ addons/_variables.tf
@@ -26,3 +26,20 @@ variable "waf_environment" {
   description = "The WAF environment name - used as part of resource name"
   type        = string
 }
+
+variable "waf_log_bucket" {
+  default     = ""
+  description = "The name of the bucket to store waf logs in"
+}
+
+variable "waf_firehose_buffer_size" {
+  type        = "string"
+  description = "Buffer incoming data to the specified size, in MBs, before delivering it to the destination. Valid value is between 64-128. Recommended is 128, specifying a smaller buffer size can result in the delivery of very small S3 objects, which are less efficient to query."
+  default     = "128"
+}
+
+variable "waf_firehose_buffer_interval" {
+  type        = "string"
+  description = "Buffer incoming data for the specified period of time, in seconds, before delivering it to the destination. Valid value is between 60-900. Smaller value makes the logs delivered faster. Bigger value increase the chance to make the file size bigger, which are more efficient to query."
+  default     = "900"
+}
