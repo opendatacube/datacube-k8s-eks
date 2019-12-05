20626b8de459acacd1631be1dd2dfcd7191b7e01
Woodcock, Robert (Mineral Resources, Black Mountain) <Robert.Woodcock@csiro.au>
2019-12-05 15:06:01 +1100

    Use the outputs from EKS module to obtain the cluster endpoint and CA for the kubernetes provider

--- infra/modules/setup/main.tf
+++ infra/modules/setup/main.tf
@@ -1,9 +1,9 @@
 data "aws_caller_identity" "current" {
 }
 
-data "aws_eks_cluster" "odc" {
-  name = var.cluster_name
-}
+#data "aws_eks_cluster" "odc" {
+#  name = var.cluster_name
+#}
 
 data "aws_eks_cluster_auth" "odc" {
   name = var.cluster_name
@@ -13,7 +13,7 @@ data "aws_eks_cluster_auth" "odc" {
 provider "kubernetes" {
   load_config_file       = false
 #  host                   = data.aws_eks_cluster.odc.endpoint
-  host                   = module.eks.api_endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.odc.certificate_authority.0.data)
+  host                   = var.cluster_endpoint
+  cluster_ca_certificate = base64decode(var.cluster_endpoint)
   token                  = data.aws_eks_cluster_auth.odc.token
 }
