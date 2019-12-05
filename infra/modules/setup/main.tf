d4de8c6d846095ee2d750c007928d6cbe5ac6126
Woodcock, Robert (Mineral Resources, Black Mountain) <Robert.Woodcock@csiro.au>
2019-12-05 14:58:29 +1100

    Need to change this from being a data provider lookup to being part of the single terraform run so tf destroys and replans correctly

--- infra/modules/setup/main.tf
+++ infra/modules/setup/main.tf
@@ -12,7 +12,8 @@ data "aws_eks_cluster_auth" "odc" {
 
 provider "kubernetes" {
   load_config_file       = false
-  host                   = "${data.aws_eks_cluster.odc.endpoint}"
-  cluster_ca_certificate = "${base64decode(data.aws_eks_cluster.odc.certificate_authority.0.data)}"
-  token                  = "${data.aws_eks_cluster_auth.odc.token}"
+#  host                   = data.aws_eks_cluster.odc.endpoint
+  host                   = module.eks.api_endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.odc.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.odc.token
 }
