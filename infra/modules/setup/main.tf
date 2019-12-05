0b020daf655debec32cace3d71610bff1371cbe9
Woodcock, Robert (Mineral Resources, Black Mountain) <Robert.Woodcock@csiro.au>
2019-12-05 12:13:32 +1100

    Removing use of local exec so module can run without a local install of aws cli (WIP)

--- infra/modules/setup/main.tf
+++ infra/modules/setup/main.tf
@@ -1,16 +1,18 @@
 data "aws_caller_identity" "current" {
 }
 
+data "aws_eks_cluster" "odc" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "odc" {
+  name = var.cluster_name
+}
+
+
 provider "kubernetes" {
   load_config_file       = false
-  host                   = var.cluster_endpoint
-  cluster_ca_certificate = base64decode(var.cluster_ca)
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    command     = "aws"
-    args        = [
-      "eks", "get-token",
-      "--cluster-name", "${var.cluster_name}"
-    ]
-  }
+  host                   = "${data.aws_eks_cluster.odc.endpoint}"
+  cluster_ca_certificate = "${base64decode(data.aws_eks_cluster.odc.certificate_authority.0.data)}"
+  token                  = "${data.aws_eks_cluster_auth.odc.token}"
 }
