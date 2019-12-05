d19ac921f67e3f07f71753204015ec6d4d563121
Woodcock, Robert (Mineral Resources, Black Mountain) <Robert.Woodcock@csiro.au>
2019-12-05 15:22:04 +1100

    Fix incorrect variable

--- infra/modules/setup/main.tf
+++ infra/modules/setup/main.tf
@@ -14,6 +14,6 @@ provider "kubernetes" {
   load_config_file       = false
 #  host                   = data.aws_eks_cluster.odc.endpoint
   host                   = var.cluster_endpoint
-  cluster_ca_certificate = base64decode(var.cluster_endpoint)
+  cluster_ca_certificate = base64decode(var.cluster_ca)
   token                  = data.aws_eks_cluster_auth.odc.token
 }
