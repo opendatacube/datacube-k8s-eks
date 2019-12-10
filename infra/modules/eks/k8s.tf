db6612b537f7aee7f4457ee6cb1069cea5b8aa44
Woodcock, Robert (Mineral Resources, Black Mountain) <Robert.Woodcock@csiro.au>
2019-12-10 12:07:29 +1100

    Pin k8s provider to avoid #127

--- infra/modules/eks/k8s.tf
+++ infra/modules/eks/k8s.tf
@@ -4,8 +4,8 @@ data "aws_eks_cluster_auth" "odc" {
 
 
 provider "kubernetes" {
+  version = "1.9"
   load_config_file       = false
-#  host                   = data.aws_eks_cluster.odc.endpoint
   host                   = aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.odc.token
