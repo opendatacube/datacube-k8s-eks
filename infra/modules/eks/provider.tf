0ea3f3454ebf0c48b649d695bf76a03a92bad029
Woodcock, Robert (Mineral Resources, Black Mountain) <Robert.Woodcock@csiro.au>
2019-12-12 10:11:44 +1100

    Force helm provider to not attempt loading of k8s config

--- infra/modules/eks/provider.tf
+++ infra/modules/eks/provider.tf
@@ -13,6 +13,7 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes {
+    load_config_file       = false
     host                   = aws_eks_cluster.eks.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.odc.token
