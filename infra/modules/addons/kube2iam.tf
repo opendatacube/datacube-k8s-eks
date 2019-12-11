b8a9bb051e39f5f580c4cdf2a278022b1af23921
Woodcock, Robert (Mineral Resources, Black Mountain) <Robert.Woodcock@csiro.au>
2019-12-11 15:47:50 +1100

    debugging circular reference temp disable

--- infra/modules/addons/kube2iam.tf
+++ infra/modules/addons/kube2iam.tf
@@ -2,23 +2,23 @@
 data "aws_caller_identity" "current" {
 }
 
-resource "helm_release" "kube2iam" {
-  name       = "kube2iam"
-  repository = "https://kubernetes-charts.storage.googleapis.com"
-  chart      = "kube2iam"
-  namespace  = "kube-system"
+# resource "helm_release" "kube2iam" {
+#   name       = "kube2iam"
+#   repository = "https://kubernetes-charts.storage.googleapis.com"
+#   chart      = "kube2iam"
+#   namespace  = "kube-system"
 
-  values = [
-    file("${path.module}/config/kube2iam.yaml"),
-  ]
+#   values = [
+#     file("${path.module}/config/kube2iam.yaml"),
+#   ]
 
-  set {
-    name  = "extraArgs.base-role-arn"
-    value = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/"
-  }
+#   set {
+#     name  = "extraArgs.base-role-arn"
+#     value = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/"
+#   }
 
-  depends_on = [
-    module.tiller,
-  ]
-}
+#   depends_on = [
+#     module.tiller,
+#   ]
+# }
 
