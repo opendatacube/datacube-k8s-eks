dc3e37862f53b38e7682cec56f1c74c71f3f3b57
Woodcock, Robert (Mineral Resources, Black Mountain) <Robert.Woodcock@csiro.au>
2019-12-12 12:03:28 +1100

    Following a pattern on a popular eks module

--- infra/modules/eks/provider.tf
+++ infra/modules/eks/provider.tf
@@ -1,12 +1,16 @@
-data "aws_eks_cluster_auth" "odc" {
-  name = aws_eks_cluster.eks.id
-}
+# data "aws_eks_cluster_auth" "odc" {
+#   name = aws_eks_cluster.eks.id
+
+#   depends_on =[
+#     aws_eks_cluster.eks,
+#   ]
+# }
 
 
-provider "kubernetes" {
-  version = "1.9"
-  load_config_file       = false
-  host                   = aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.odc.token
-}
+# provider "kubernetes" {
+#   version = "1.9"
+#   load_config_file       = false
+#   host                   = aws_eks_cluster.eks.endpoint
+#   cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.odc.token
+# }
