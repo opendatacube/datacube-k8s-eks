ceae59f90e9066f17543f70a8550da7c578ab0a0
Santosh Mohan <santosh.mohan@ga.gov.au>
2019-12-16 11:02:33 +1100

    Make data resource optional

--- addons/node_localdns_cache.tf
+++ addons/node_localdns_cache.tf
@@ -1,4 +1,4 @@
-#Ref: https://aws.amazon.com/blogs/containers/eks-dns-at-scale-and-spikeiness/
+# Ref: https://aws.amazon.com/blogs/containers/eks-dns-at-scale-and-spikeiness/
 
 variable "localdns_cache_enable" {
   type = bool
@@ -21,16 +21,21 @@ variable "node_local_dns" {
 variable "node_dns_server" {
   type = string
   description = "kube-dns service IP"
-  #default = $(kubectl get svc -n kube-system kube-dns -o jsonpath='{.spec.clusterIP}')
+
+  # Default ip address can be read from $(kubectl get svc -n kube-system kube-dns -o jsonpath='{.spec.clusterIP}')
   default = "172.20.0.10"
 }
 
 data "http" "nodelocaldns" {
+  count    = var.localdns_cache_enable ? 1 : 0
+
   # Refernce nodelocaldns file via raw url
   url = "https://raw.githubusercontent.com/kubernetes/kubernetes/master/cluster/addons/dns/nodelocaldns/nodelocaldns.yaml"
 }
 
 data "null_data_source" "dns_domain" {
+  count    = var.localdns_cache_enable ? 1 : 0
+
   inputs = {
     # Substitute __PILLAR__DNS__DOMAIN__ variable with node_dns_domain variable
     text = replace("${data.http.nodelocaldns.body}", "__PILLAR__DNS__DOMAIN__", "${var.node_dns_domain}")
@@ -38,6 +43,8 @@ data "null_data_source" "dns_domain" {
 }
 
 data "null_data_source" "local_dns" {
+  count    = var.localdns_cache_enable ? 1 : 0
+
   inputs = {
     # Substitute __PILLAR__LOCAL__DNS__ variable with node_local_dns variable
     text = replace("${data.null_data_source.dns_domain.outputs["text"]}", "__PILLAR__LOCAL__DNS__", "${var.node_local_dns}")
@@ -45,6 +52,8 @@ data "null_data_source" "local_dns" {
 }
 
 data "template_file" "localdns_template" {
+  count    = var.localdns_cache_enable ? 1 : 0
+
   # Substitute __PILLAR__DNS__SERVER__ variable with node_dns_server variable
   template = replace("${data.null_data_source.local_dns.outputs["text"]}", "__PILLAR__DNS__SERVER__", "${var.node_dns_server}")
 }
