8053ec78cb09c1fa1179f2128b0ad6678ab8d7b9
Woodcock, Robert (Mineral Resources, Black Mountain) <Robert.Woodcock@csiro.au>
2019-12-18 13:51:58 +1100

    Fix up reference to triggers

--- odc_k8s/flux.tf
+++ odc_k8s/flux.tf
@@ -98,12 +98,12 @@ resource "null_resource" "apply_flux_crd" {
 
   provisioner "local-exec" {
     interpreter = [var.local_exec_interpreter, "-c"]
-    command = join("\n", [self.install_kubectl, "kubectl apply -f https://raw.githubusercontent.com/fluxcd/flux/helm-0.10.1/deploy-helm/flux-helm-release-crd.yaml"])
+    command = join("\n", [self.triggers.install_kubectl, "kubectl apply -f https://raw.githubusercontent.com/fluxcd/flux/helm-0.10.1/deploy-helm/flux-helm-release-crd.yaml"])
   }
 
   provisioner "local-exec" {
     when    = destroy
-    command = join("\n", [self.install_kubectl, "kubectl destroy -f https://raw.githubusercontent.com/fluxcd/flux/helm-0.10.1/deploy-helm/flux-helm-release-crd.yaml"])
+    command = join("\n", [self.triggers.install_kubectl, "kubectl destroy -f https://raw.githubusercontent.com/fluxcd/flux/helm-0.10.1/deploy-helm/flux-helm-release-crd.yaml"])
   }
 
 }
