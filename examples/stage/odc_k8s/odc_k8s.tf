336646ea8ababfff3eef77233fc0797b97e09d05
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2019-12-20 15:04:55 +1100

    test with flux-odc-sample repo

--- examples/stage/odc_k8s/odc_k8s.tf
+++ examples/stage/odc_k8s/odc_k8s.tf
@@ -35,18 +35,18 @@ module "odc_k8s" {
   db_admin_password = data.terraform_remote_state.odc_eks-stage.outputs.db_admin_username
 
   # Setup Flux/FluxCloud
+  flux_enabled = false
+  flux_git_repo_url = "https://github.com/opendatacube/flux-odc-sample"
+  flux_git_branch = "master"
+  flux_git_path = "flux"
+  #flux_git_label = "flux-sync"
+
   fluxcloud_enabled = false
-  fluxcloud_slack_url = "" # "https://hooks.slack.com/services/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"
+  fluxcloud_slack_url = "" # "https://hooks.slack.com/services/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"
   fluxcloud_slack_channel = "" # "#ga-wms-updates"
-  fluxcloud_slack_name = "Flux Deployer"
+  fluxcloud_slack_name = "Flux Example Deployer"
   fluxcloud_slack_emoji = ":zoidberg:"
   fluxcloud_github_url = "https://github.com/opendatacube/flux-odc-sample"
   fluxcloud_commit_template = "{{ .VCSLink }}/commits/{{ .Commit }}"
-
-  # Setup Flux CD
-  flux_enabled = false
-  #flux_git_repo_url = "<repo goes here>"
-  #flux_git_branch = "master"
-  #flux_git_label = "flux-sync"
 
 }
