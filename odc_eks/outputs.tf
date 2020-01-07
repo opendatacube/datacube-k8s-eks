19b8ee7b742c871f976d3e9fa9b5e5823ac4f9e4
Woodcock, Robert (Mineral Resources, Black Mountain) <Robert.Woodcock@csiro.au>
2020-01-07 16:05:35 +1100

    Add cluster_arn to outputs

--- odc_eks/outputs.tf
+++ odc_eks/outputs.tf
@@ -7,6 +7,10 @@ output "cluster_id" {
   value = module.eks.cluster_id
 }
 
+output "cluster_arn" {
+  value = module.eks.cluster_arn
+}
+
 output "cluster_role" {
   value = module.eks.user_role_arn
 }
