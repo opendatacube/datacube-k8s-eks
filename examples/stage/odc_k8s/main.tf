d1997075f09255071d5983675a3ac7d094a943ed
whatnick <tisham@whatnick.com>
2020-01-09 06:55:00 +0000

    #168 Attempt to init with local backend for CI

--- examples/stage/odc_k8s/main.tf
+++ examples/stage/odc_k8s/main.tf
@@ -1,7 +1,11 @@
 terraform {
-  backend "s3" {
-    bucket = "odc-test-stage-backend-tfstate"
-    key    = "odc_k8s_terraform.tfstate"
-    region = "ap-southeast-2"
+  #backend "s3" {
+  #  bucket = "odc-test-stage-backend-tfstate"
+  #  key    = "odc_k8s_terraform.tfstate"
+  #  region = "ap-southeast-2"
+  #}
+
+  backend "local" {
+    path = "ci-test/terraform.tfstate"
   }
 }
