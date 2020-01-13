2ff5c9f046b926d1e57f6f4052ff99379686f522
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2020-01-14 08:36:30 +1100

    use two different tf file based on application group to provision role

--- examples/stage/odc_apps_k8s/odc_roles_admin.tf
+++ examples/stage/odc_apps_k8s/odc_roles_admin.tf
@@ -1,8 +1,8 @@
-# Roles for k8s applications
+# Roles for k8s admin applications
 # Separate TF files can be used per application but in some cases it
-# is more manageable to simply group them up (e.g. Use the odc_k8s_roles and a list of roles)
+# is more manageable to simply group them up (e.g. Use the odc_roles and a list of roles)
 
-module "odc_k8s_roles" {
+module "odc_admin_roles" {
 //  source = "github.com/opendatacube/datacube-k8s-eks//odc_roles?ref=terraform-aws-odc"
   source = "../../../odc_roles"
 
@@ -214,75 +214,6 @@ module "odc_k8s_roles" {
             ],
             "Resource": [
               "*"
-            ]
-          }
-        ]
-      }
-      EOF
-    },
-    {
-      name = "wms"
-      policy = <<-EOF
-      {
-        "Version": "2012-10-17",
-        "Statement": [
-          {
-            "Effect": "Allow",
-            "Action": ["S3:ListBucket"],
-            "Resource": [
-              "arn:aws:s3:::dea-public-data"
-            ]
-          },
-          {
-            "Effect": "Allow",
-            "Action": ["S3:GetObject"],
-            "Resource": [
-              "arn:aws:s3:::dea-public-data/*"
-            ]
-          },
-          {
-            "Effect": "Allow",
-            "Action": ["s3:*"],
-            "Resource": [
-              "arn:aws:s3:::datacube-index-dump/*"
-            ]
-          }
-        ]
-      }
-      EOF
-    },
-    {
-      name  = "wps"
-      policy = <<-EOF
-      {
-        "Version": "2012-10-17",
-        "Statement": [
-          {
-            "Effect": "Allow",
-            "Action": ["S3:ListBucket"],
-            "Resource": [
-              "arn:aws:s3:::dea-public-data"
-            ]
-          },
-          {
-            "Effect": "Allow",
-            "Action": ["S3:GetObject"],
-            "Resource": [
-              "arn:aws:s3:::dea-public-data/*"
-            ]
-          },
-          {
-            "Effect": "Allow",
-            "Action": [
-              "s3:PutObject",
-              "s3:GetObjectAcl",
-              "s3:GetObject",
-              "s3:ListBucket",
-              "s3:PutObjectAcl"
-            ],
-            "Resource": [
-              "arn:aws:s3:::dea-wps-results",
-              "arn:aws:s3:::dea-wps-results/*"
             ]
           }
         ]
