fd0a876610ed0dc501ccbe245088844ab8ee69ed
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2020-01-20 12:51:25 +1100

    code cleanup

--- examples/stage/odc_eks/main.tf
+++ examples/stage/odc_eks/main.tf
@@ -3,8 +3,5 @@ terraform {
     bucket = "odc-test-stage-backend-tfstate"
     key    = "odc_eks_terraform.tfstate"
     region = "ap-southeast-2"
-    dynamodb_table = "odc-test-stage-backend-terraform-lock"
-    # Force encryption
-    encrypt = true
   }
 }
