c2ba2de6051ebb55c4221e971ef90aa63bb0c7c4
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2020-01-21 08:57:57 +1100

    remove duplication

--- odc_eks/main.tf
+++ odc_eks/main.tf
@@ -130,14 +130,3 @@ module "jhub_cognito_auth" {
   namespace   = var.namespace
   environment = var.environment
 }
-
-module "jhub_cognito_auth" {
-  source               = "./modules/cognito"
-
-  cognito_auth_enabled = var.jhub_cognito_auth_enabled
-  auto_verify          = var.cognito_auto_verify
-  user_pool_name       = "${module.odc_eks_label.id}-jhub-userpool"
-  user_pool_domain     = "${module.odc_eks_label.id}-jhub-auth"
-  callback_url         = "https://${var.app_name}.${var.domain_name}/oauth_callback"
-  user_groups          = var.jhub_cognito_user_groups
-}
