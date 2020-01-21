8303664cd9ca636c700025bfb6480956dae621d4
Woodcock, Robert (Mineral Resources, Black Mountain) <Robert.Woodcock@csiro.au>
2020-01-21 15:57:43 +1100

    Yes, I know, I got interrupted is my excuse

--- cognito/output.tf
+++ cognito/output.tf
@@ -1,22 +1,22 @@
 output "userpool" {
-  value = aws_cognito_user_pool.pool.name : null
+  value = aws_cognito_user_pool.pool.name
 }
 
 output "userpool_id" {
-  value = aws_cognito_user_pool.pool.id : null
+  value = aws_cognito_user_pool.pool.id
   sensitive = true
 }
 
 output "userpool_domain" {
-  value = aws_cognito_user_pool_domain.domain.domain : null
+  value = aws_cognito_user_pool_domain.domain.domain
 }
 
 output "client_id" {
-  value = aws_cognito_user_pool_client.client.id : null
+  value = aws_cognito_user_pool_client.client.id
   sensitive = true
 }
 
 output "client_secret" {
-  value = aws_cognito_user_pool_client.client.client_secret : null
+  value = aws_cognito_user_pool_client.client.client_secret
   sensitive = true
 }
