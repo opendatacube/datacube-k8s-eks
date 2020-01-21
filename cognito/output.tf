fcf814e762dcd644c5b80a2473bc2e3fbbba8e4f
Woodcock, Robert (Mineral Resources, Black Mountain) <Robert.Woodcock@csiro.au>
2020-01-21 15:53:06 +1100

    Outputs no longer arrays (count removed)

--- cognito/output.tf
+++ cognito/output.tf
@@ -1,22 +1,22 @@
 output "userpool" {
-  value = aws_cognito_user_pool.pool[0].name : null
+  value = aws_cognito_user_pool.pool.name : null
 }
 
 output "userpool_id" {
-  value = aws_cognito_user_pool.pool[0].id : null
+  value = aws_cognito_user_pool.pool.id : null
   sensitive = true
 }
 
 output "userpool_domain" {
-  value = aws_cognito_user_pool_domain.domain[0].domain : null
+  value = aws_cognito_user_pool_domain.domain.domain : null
 }
 
 output "client_id" {
-  value = aws_cognito_user_pool_client.client[0].id : null
+  value = aws_cognito_user_pool_client.client.id : null
   sensitive = true
 }
 
 output "client_secret" {
-  value = aws_cognito_user_pool_client.client[0].client_secret : null
+  value = aws_cognito_user_pool_client.client.client_secret : null
   sensitive = true
 }
