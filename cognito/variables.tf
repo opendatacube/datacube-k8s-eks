3ccdda347557afb28c3ebb3edb0c0472c54e07bb
Woodcock, Robert (Mineral Resources, Black Mountain) <Robert.Woodcock@csiro.au>
2020-01-21 16:00:37 +1100

    Remove unused variable

--- cognito/variables.tf
+++ cognito/variables.tf
@@ -1,8 +1,3 @@
-variable "cognito_auth_enabled" {
-  default = false
-  description = "Whether the cognito user pool should be created"
-}
-
 variable "callback_url" {
   type = string
   description = "The callback url for your application"
