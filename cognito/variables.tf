763320b06abc021a10eec6dd1cbde42fa8a7b936
Rob Woodcock <woodcockr@users.noreply.github.com>
2020-01-28 13:31:15 +1100

    Additional config for cognito (#183)
    * Add additional configuration to customise emails
    
    Includes workaround:
    * AWS made a change that is breaking the Terraform provider and some magic is required to restore operations. https://github.com/terraform-providers/terraform-provider-aws/pull/8845#issuecomment-498720657
    * Add a lifecycle hack to stop TF from mucking with the invalid password policy change because AWS changed stuff and TF AWS provider is still being updated
    
    
    
    Co-authored-by: Nikita <nikita.r.gandhi@gmail.com>

--- cognito/variables.tf
+++ cognito/variables.tf
@@ -42,3 +42,54 @@ variable "namespace" {
 
 variable "owner" {
 }
+
+# admin_create_user_config
+variable "admin_create_user_config" {
+  description = "The configuration for AdminCreateUser requests"
+  type        = map
+  default     = {}
+}
+
+variable "admin_create_user_config_allow_admin_create_user_only" {
+  description = "Set to True if only the administrator is allowed to create user profiles. Set to False if users can sign themselves up via an app"
+  type        = bool
+  default     = false
+}
+
+variable "admin_create_user_config_unused_account_validity_days" {
+  description = "The user account expiration limit, in days, after which the account is no longer usable"
+  type        = number
+  default     = 0
+}
+
+variable "admin_create_user_config_email_message" {
+  description = "The message template for email messages. Must contain `{username}` and `{####}` placeholders, for username and temporary password, respectively"
+  type        = string
+  default     = null
+}
+
+
+variable "admin_create_user_config_email_subject" {
+  description = "The subject line for email messages"
+  type        = string
+  default     = null
+}
+
+variable "admin_create_user_config_sms_message" {
+  description = "- The message template for SMS messages. Must contain `{username}` and `{####}` placeholders, for username and temporary password, respectively"
+  type        = string
+  default     = null
+}
+
+variable "email_verification_message" {
+  description = "A string representing the email verification message"
+  type        = string
+  default     = null
+}
+
+variable "email_verification_subject" {
+  description = "A string representing the email verification subject"
+  type        = string
+  default     = null
+}
+
