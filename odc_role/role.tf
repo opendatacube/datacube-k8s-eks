b239e867ccc205319fccb7e9cabf15f04735ae5d
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2020-02-05 12:25:09 +1100

    update odc_role module to support additional tags

--- odc_role/role.tf
+++ odc_role/role.tf
@@ -4,37 +4,39 @@ data "aws_caller_identity" "current" {
 resource "aws_iam_role" "role" {
   name  = var.role.name
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
+  assume_role_policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "",
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
       },
-      "Action": "sts:AssumeRole"
-    },
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/nodes.${var.cluster_id}"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-
-  tags = {
-    Name        = var.role.name
-    Cluster     = var.cluster_id
-    Owner       = var.owner
-    Namespace   = var.namespace
-    Environment = var.environment
+      {
+        "Sid": "",
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/nodes.${var.cluster_id}"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
   }
+  EOF
+
+  tags = merge(
+    {
+      name = var.role.name
+      owner = var.owner
+      namespace = var.namespace
+      environment = var.environment
+    },
+    var.tags
+  )
 }
 
 resource "aws_iam_role_policy" "role_policy" {
