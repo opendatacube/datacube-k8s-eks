1d490e9c8dc5ace95adf2b16a43f76f36af696e8
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2020-02-19 13:58:53 +1100

    put a comment

--- addons/calico.tf
+++ addons/calico.tf
@@ -1,12 +1,14 @@
-# Install calico using helm release: https://github.com/aws/eks-charts/tree/master/stable/aws-vpc-cni
-
 variable "calico_enabled" {
   default     = false
   description = "Whether to install calico to your EKS cluster"
 }
 
+# Install calico using helm release: https://github.com/aws/eks-charts/tree/master/stable/aws-vpc-cni
+# TODO: Getting error installing calico using helm install so used null_resource instead
+#        helm install calico-node --namespace kube-system eks/aws-calico
+#        Error: failed to install CRD crds/kustomization.yaml: unable to recognize "": no matches for kind "Kustomization" in version "kustomize.config.k8s.io/v1beta1"
 //resource "helm_release" "calico" {
-//  count      = var.calico_enable ? 1 : 0
+//  count      = var.calico_enabled ? 1 : 0
 //  name       = "calico-node"
 //  repository = "eks"
 //  chart      = "eks/aws-calico"
