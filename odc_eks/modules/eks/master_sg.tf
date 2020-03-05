b7097f0c1e81f715f868cfc1b473f0232eb5d602
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2020-03-05 18:52:56 +1100

    slide adjustment to eks cluster and node security group

--- odc_eks/modules/eks/master_sg.tf
+++ odc_eks/modules/eks/master_sg.tf
@@ -3,13 +3,6 @@ resource "aws_security_group" "eks_cluster" {
   description = "Cluster communication with worker nodes"
   vpc_id      = var.vpc_id
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = merge(
     {
       Name = "${var.cluster_id}-cluster-sg"
@@ -21,15 +14,35 @@ resource "aws_security_group" "eks_cluster" {
   )
 }
 
-# Converts admin_access_CIDRs to descriptions / IP CIDRs
-resource "aws_security_group_rule" "eks-cluster-ingress-workstation-https" {
-  count = length(var.admin_access_CIDRs)
-  cidr_blocks       = [element(values(var.admin_access_CIDRs), count.index)]
-  description       = element(keys(var.admin_access_CIDRs), count.index)
-  from_port         = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.eks_cluster.id
-  to_port           = 443
-  type              = "ingress"
+resource "aws_security_group_rule" "eks_cluster_ingress_node_https" {
+  description              = "Allow worker nodes to communicate with control pane"
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_cluster.id
+  source_security_group_id = aws_security_group.eks_node.id
 }
 
+# Converts admin_access_CIDRs to descriptions / IP CIDRs
+resource "aws_security_group_rule" "eks_cluster_ingress_workstation_https" {
+  count             = length(var.admin_access_CIDRs)
+  description       = element(keys(var.admin_access_CIDRs), count.index)
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.eks_cluster.id
+  cidr_blocks       = [element(values(var.admin_access_CIDRs), count.index)]
+}
+
+# Security group - outbound
+resource "aws_security_group_rule" "eks_cluster_egress_node" {
+  description              = "Allow cluster control pane to communicate with worker nodes"
+  type                     = "egress"
+  from_port                = 1025
+  to_port                  = 65535
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_cluster.id
+  source_security_group_id = aws_security_group.eks_node.id
+}
