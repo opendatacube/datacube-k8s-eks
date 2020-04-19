3ee7a0849fbe3ffe4e9195a59c63f58338d30572
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2020-04-20 09:58:12 +1000

    add merged ingress tf config - used for merging ingresses under same namespace rather than use separate ingresses

---
+++ examples/stage/03_odc_apps_k8s/merge_ingress.tf
@@ -0,0 +1,17 @@
+data "template_file" "merge_ingress" {
+  template = file("${path.module}/config/merge_ingress.yaml")
+  vars = {
+    certificate_arn = local.certificate_arn
+  }
+}
+
+resource "kubernetes_config_map" "merge_ingress" {
+  metadata {
+    name = "merged-ingress"
+    namespace = kubernetes_namespace.web.metadata[0].name
+  }
+
+  data = {
+    "annotations" = data.template_file.merge_ingress.rendered
+  }
+}
