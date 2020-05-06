60b78581a6b85c85942fe493bd4ed414e2a55713
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2020-05-06 11:18:21 +1000

    fix typo

--- odc_k8s/aws_auth.tf
+++ odc_k8s/aws_auth.tf
@@ -2,8 +2,8 @@ locals {
   default_user_configmap_template = length(var.users) > 0 ? data.template_file.map_user_config.rendered : null
   mapUsers = var.user_config_template != "" ? var.user_config_template : local.default_user_configmap_template
 
-  default_user_configmap_template = length(var.node_roles) > 0 ? data.template_file.map_role_config.rendered : null
-  mapRoles = var.role_config_template != "" ? var.role_config_template : local.default_user_configmap_template
+  default_role_configmap_template = length(var.node_roles) > 0 ? data.template_file.map_role_config.rendered : null
+  mapRoles = var.role_config_template != "" ? var.role_config_template : local.default_role_configmap_template
 }
 
 data "template_file" "map_user_config" {
