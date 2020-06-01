4694e0c4933c32318070c8a6e83cb0af413a6c3f
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2020-06-01 12:59:22 +1000

    remove cilium and install-extra-software library

--- odc_k8s/flux_helm_operator.tf
+++ odc_k8s/flux_helm_operator.tf
@@ -27,36 +27,17 @@ resource "helm_release" "flux_helm_operator" {
   }
 }
 
-# installing extra softwares + helm-operator CRD
+# installing helm-operator CRD
 resource "null_resource" "apply_flux_helm_operator_crd" {
   count = var.flux_enabled ? 1 : 0
 
   triggers = {
     cluster_updated              = var.cluster_id
     kubernetes_namespace_updated = kubernetes_namespace.flux[0].metadata[0].name
-
-    # Special trigger: When using null_resource, you can use the triggers map both to signal when the provisioners
-    # need to re-run (the usual purpose as above) and to retain values you can access via self during the destroy phase.
-    # This avoids dependency issues during the destory phase
-    install_kubectl        = local.install_kubectl
-    local_exec_interpreter = var.local_exec_interpreter
+    flux_helm_operator_version   = var.flux_helm_operator_version
   }
-
-  depends_on = [
-    kubernetes_namespace.flux,
-    #    kubernetes_config_map.aws_auth,
-  ]
 
   provisioner "local-exec" {
-    interpreter = [self.triggers.local_exec_interpreter, "-c"]
-    command     = join("\n", [self.triggers.install_kubectl, "kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/${var.flux_helm_operator_version}/deploy/crds.yaml"])
+    command = "aws eks --region ${var.region} update-kubeconfig --name ${var.cluster_id} && kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/${var.flux_helm_operator_version}/deploy/crds.yaml"
   }
-
-
-  provisioner "local-exec" {
-    when        = destroy
-    interpreter = [self.triggers.local_exec_interpreter, "-c"]
-    command     = join("\n", [self.triggers.install_kubectl, "kubectl delete -f https://raw.githubusercontent.com/fluxcd/helm-operator/${var.flux_helm_operator_version}/deploy/crds.yaml"])
-  }
-
 }
