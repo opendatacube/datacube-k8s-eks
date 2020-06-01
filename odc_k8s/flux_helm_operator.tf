dcc30ab9aa2d6c10ab59695b928a59aa90640eb7
Nikita Gandhi <nikita.r.gandhi@gmail.com>
2020-06-01 13:19:09 +1000

    create a variable to pass kubeconfig command

--- odc_k8s/flux_helm_operator.tf
+++ odc_k8s/flux_helm_operator.tf
@@ -8,6 +8,12 @@ variable "enabled_helm_versions" {
   default     = "v2\\,v3"
 }
 
+variable "kube_config_cmd" {
+  type        = string
+  description = "Provide kubeconfig command for connecting to your cluster"
+  default     = ""
+}
+
 resource "helm_release" "flux_helm_operator" {
   count      = var.flux_enabled ? 1 : 0
   name       = "helm-operator"
@@ -27,6 +33,11 @@ resource "helm_release" "flux_helm_operator" {
   }
 }
 
+locals {
+  default_kube_config_cmd = "aws eks --region ${var.region} update-kubeconfig --name ${var.cluster_id}"
+  kubectl_config_cmd      = var.kube_config_cmd == "" ? local.default_kube_config_cmd : var.kube_config_cmd
+}
+
 # installing helm-operator CRD
 resource "null_resource" "apply_flux_helm_operator_crd" {
   count = var.flux_enabled ? 1 : 0
@@ -35,9 +46,10 @@ resource "null_resource" "apply_flux_helm_operator_crd" {
     cluster_updated              = var.cluster_id
     kubernetes_namespace_updated = kubernetes_namespace.flux[0].metadata[0].name
     flux_helm_operator_version   = var.flux_helm_operator_version
+    kubectl_config_cmd           = local.kubectl_config_cmd
   }
 
   provisioner "local-exec" {
-    command = "aws eks --region ${var.region} update-kubeconfig --name ${var.cluster_id} && kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/${var.flux_helm_operator_version}/deploy/crds.yaml"
+    command = join("\n", [self.triggers.kubectl_config_cmd, "kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/${var.flux_helm_operator_version}/deploy/crds.yaml"])
   }
 }
