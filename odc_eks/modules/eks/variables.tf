0fdeb515e634517ff93788bb8e3639bc57b4015e
Robert Woodcock <Robert.Woodcock@csiro.au>
2020-11-07 15:00:25 +1100

    Add EKS endpoint ready check via local-exec to ensure its ready

--- odc_eks/modules/eks/variables.tf
+++ odc_eks/modules/eks/variables.tf
@@ -114,3 +114,15 @@ variable "tags" {
   description = "Additional tags (e.g. `map('StackName','XYZ')`"
   default     = {}
 }
+
+variable "wait_for_cluster_cmd" {
+  description = "Custom local-exec command to execute for determining if the eks cluster is healthy. Cluster endpoint will be available as an environment variable called ENDPOINT"
+  type        = string
+  default     = "for i in `seq 1 60`; do if `command -v wget > /dev/null`; then wget --no-check-certificate -O - -q $ENDPOINT/healthz >/dev/null && exit 0 || true; else curl -k -s $ENDPOINT/healthz >/dev/null && exit 0 || true;fi; sleep 5; done; echo TIMEOUT && exit 1"
+}
+
+variable "wait_for_cluster_interpreter" {
+  description = "Custom local-exec command line interpreter for the command to determining if the eks cluster is healthy."
+  type        = list(string)
+  default     = ["/bin/sh", "-c"]
+}
