a3df93dabfc0be57bed5da4190a8c5dbf6088799
Tisham Dhar <tisham.dhar@ga.gov.au>
2020-11-26 05:28:00 +0000

    Fix format
    Signed-off-by: Tisham Dhar <tisham.dhar@ga.gov.au>

--- cognito/pinpoint.tf
+++ cognito/pinpoint.tf
@@ -1,14 +1,14 @@
 # Create a PinPoint app for every app client
 resource "aws_pinpoint_app" "pinpoint_app" {
-  count     = var.enable_pinpoint ? 1 : 0
-  for_each  = var.app_clients
-  name      = each.key
+  count    = var.enable_pinpoint ? 1 : 0
+  for_each = var.app_clients
+  name     = each.key
 }
 
 # Conditionally create role if pin point is enabled in module
 resource "aws_iam_role" "pinpoint_role" {
   count = var.enable_pinpoint ? 1 : 0
-  name = "pinpoint-role"
+  name  = "pinpoint-role"
 
   assume_role_policy = <<EOF
 {
