3489753e2b66c876805b815cefd597f956e37003
Mike Mochan <mmochan@mac.com>
2021-03-12 16:59:24 +1000

    Add autoscaling_group for Bottlerocket OS

--- odc_eks/modules/eks/workers.tf
+++ odc_eks/modules/eks/workers.tf
@@ -168,3 +168,88 @@ resource "aws_autoscaling_group" "spot_nodes" {
   depends_on = [aws_launch_template.spot]
 }
 
+resource "aws_autoscaling_group" "bottlerocket_spot_nodes" {
+  count               = var.bottlerocket_nodes_enabled ? 1 : 0
+  desired_capacity    = var.desired_nodes
+  max_size            = var.max_spot_nodes
+  min_size            = var.min_spot_nodes
+  name                = "${var.node_group_name}-${aws_launch_template.spot[0].id}-spot"
+  vpc_zone_identifier = var.eks_subnet_ids
+
+  # Don't reset to default size every time terraform is applied
+  lifecycle {
+    ignore_changes        = [desired_capacity]
+    create_before_destroy = true
+  }
+
+  launch_template {
+    id      = aws_launch_template.spot[0].id
+    version = aws_launch_template.spot[0].latest_version
+  }
+
+  # Use a dyanmic tag block rather than tags = [<list of tags>] to workaround this issue https://github.com/hashicorp/terraform-provider-aws/issues/14085
+  dynamic "tag" {
+    for_each = concat(
+      flatten([
+        for key in keys(var.bottlerocket_node_extra_tags) :
+        {
+          key                 = key
+          value               = var.bottlerocket_node_extra_tags[key]
+          propagate_at_launch = true
+        }
+      ]),
+      [
+        {
+          key                 = "Name"
+          value               = "${var.node_group_name}-${aws_launch_template.spot[0].id}-spot"
+          propagate_at_launch = true
+        },
+        {
+          key                 = "environment"
+          value               = var.environment
+          propagate_at_launch = true
+        },
+        {
+          key                 = "k8s.io/cluster-autoscaler/${aws_eks_cluster.eks.id}"
+          value               = "owned"
+          propagate_at_launch = true
+        },
+        {
+          key                 = "k8s.io/cluster-autoscaler/enabled"
+          value               = "true"
+          propagate_at_launch = true
+        },
+        {
+          key                 = "k8s.io/cluster-autoscaler/node-template/label/nodetype"
+          value               = "spot"
+          propagate_at_launch = true
+        },
+        {
+          key                 = "kubernetes.io/cluster/${aws_eks_cluster.eks.id}"
+          value               = "owned"
+          propagate_at_launch = true
+        },
+        {
+          key                 = "namespace"
+          value               = var.namespace
+          propagate_at_launch = true
+        },
+        {
+          key                 = "owner"
+          value               = var.owner
+          propagate_at_launch = true
+        },
+    ])
+    content {
+      key                 = tag.value.key
+      value               = tag.value.value
+      propagate_at_launch = tag.value.propagate_at_launch
+    }
+  }
+
+
+  # Don't break cluster autoscaler
+  suspended_processes = ["AZRebalance"]
+
+  depends_on = [aws_launch_template.spot]
+}
