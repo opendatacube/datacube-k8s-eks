c4b9b3fdc005bac413ac8571550699038cd337ca
Ralph Frederick <ralph.frederick@weyerhaeuser.com>
2021-10-01 00:20:13 -0700

    public subnet output

--- odc_eks/outputs.tf
+++ odc_eks/outputs.tf
@@ -68,6 +68,11 @@ output "private_subnets" {
   value = var.create_vpc ? module.vpc[0].private_subnets : var.private_subnets
 }
 
+output "public_subnets" {
+  value = var.create_vpc ? module.vpc[0].public_subnets : var.public_subnets
+}
+
+
 output "public_route_table_ids" {
   value = var.create_vpc ? module.vpc[0].public_route_table_ids : var.public_route_table_ids
 }
