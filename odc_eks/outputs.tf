0d748f779fbc9235e5a625f42f7a27f1637445d1
Ralph Frederick <ralph.frederick@weyerhaeuser.com>
2021-10-03 20:07:20 -0700

    Adding private_route_table_ids output

--- odc_eks/outputs.tf
+++ odc_eks/outputs.tf
@@ -75,3 +75,7 @@ output "public_subnets" {
 output "public_route_table_ids" {
   value = var.create_vpc ? module.vpc[0].public_route_table_ids : var.public_route_table_ids
 }
+
+output "private_route_table_ids" {
+  value = var.create_vpc ? module.vpc[0].private_route_table_ids : var.private_route_table_ids
+}
