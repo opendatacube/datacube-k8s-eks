c4c5ce286de0a3d821afa823d835c99f8a70d460
Dominic Schaff <dominic@schaff.dev>
2021-10-05 08:39:35 +0200

    Add changes

--- examples/stage/03_odc_apps_k8s/provider.tf
+++ examples/stage/03_odc_apps_k8s/provider.tf
@@ -1,12 +1,12 @@
 provider "aws" {
   region      = data.terraform_remote_state.odc_eks-stage.outputs.region
   max_retries = 10
+  # skip region validation until terraform provider supports this new region
+  skip_region_validation = true
 }
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-  version                = "~> 1.10"
 }
