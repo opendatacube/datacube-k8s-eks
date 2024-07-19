fdf968b45681e7f2339fb8632e08eded867b1f9e
Stacy Horton <stacy.horton@ga.gov.au>
2024-07-19 12:04:07 +1000

    This is required to match previous functionality

--- odc_eks/modules/eks/main.tf
+++ odc_eks/modules/eks/main.tf
@@ -12,6 +12,7 @@ resource "aws_eks_cluster" "eks" {
 
   access_config {
     authentication_mode = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
   }
 
   depends_on = [
