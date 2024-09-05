504b2753559d2ad25a971b8338106dc10de44180
lars-fillmore <97861771+lars-fillmore@users.noreply.github.com>
2024-09-05 08:36:59 +0000

    Add additional condition

--- odc_eks/main.tf
+++ odc_eks/main.tf
@@ -73,7 +73,7 @@ module "vpc" {
   flow_log_max_aggregation_interval = (var.create_vpc_flow_logs) ? var.flow_log_max_aggregation_interval : null
   flow_log_traffic_type             = (var.create_vpc_flow_logs) ? var.flow_log_traffic_type : null
   flow_log_file_format              = (var.create_vpc_flow_logs) ? var.flow_log_file_format : null
-  flow_log_destination_arn          = (var.create_vpc_flow_logs) ? "arn:aws:s3:::${var.flow_log_s3_bucket_name}" : null
+  flow_log_destination_arn          = (var.create_vpc_flow_logs) ? (var.create_flow_log_s3_bucket ? module.s3_bucket.arn : "arn:aws:s3:::${var.flow_log_s3_bucket_name}") : null
   
   vpc_flow_log_tags = merge(
     {
