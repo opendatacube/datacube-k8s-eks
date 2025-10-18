7803c40ced3f6243536ccc647e6670456c667881
Bskr-P <bhaskar1994p@gmail.com>
2025-10-18 05:32:25 +0530

    add output for al2023

--- odc_eks/outputs.tf
+++ odc_eks/outputs.tf
@@ -43,6 +43,10 @@ output "ami_image_id" {
   value = module.eks.ami_image_id
 }
 
+output "al2023_enabled" {
+  value = var.al2023_enabled
+}
+
 # The length check here is to prevent destroy plans referencing the wildcard_cert[0] value which doesn't exist on destroy
 output "certificate_arn" {
   value = (var.create_certificate && (length(aws_acm_certificate.wildcard_cert) > 0)) ? aws_acm_certificate.wildcard_cert[0].arn : null
